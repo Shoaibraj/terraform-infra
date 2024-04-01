6542b2e6546415fff8bd0cda825458deaa6e8b94
Shoaibraj <153000225+Shoaibraj@users.noreply.github.com>
2024-04-01 15:31:19 +0530

    docker.sh

---
+++ docker.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+apt install openjdk-11-jre-headless -y
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+sudo apt-get install jenkins -y
+systemctl start jwnkins
+systemctl enable jenkins
+systemctl status jenkins
